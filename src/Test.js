cfa478edf6a21251b66d0b9e032a3cc7bae8959b
Zleepless <donutsepia@gmail.com>
2022-03-18 14:15:26 +0700

    Prepare for reminiscence

---
+++ src/Test.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Test() {
+  return (
+    <div>
+          Testing the function and old stuff
+    </div>
+  );
+}
+
+export default Test;
