12f805d50e24fbc1c30d5d48ecbc2bce33a952c5
Zleepless <donutsepia@gmail.com>
2022-03-23 22:25:58 +0700

    Revert Plugin Test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,39 +4,7 @@ import Logo from '../assets/34.png'
 
 const Navbar = () => {
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]">
-      <div>
-        <img src={Logo} alt="Logo Image" style={{ width: '50px' }} />
-      </div>
-
-      {/* Navbar */}
-      <div>
-        <ul className='flex'>
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Work</li>
-          <li>Contact</li>
-        </ul>
-      </div>
-
-      {/* Hamburger */}
-      <div className='hidden'>
-        <Fabars />
-      </div>
-
-      {/* Mobile Menu */}
-      <ul className='hidden'>
-        <li>Home</li>
-        <li>About</li>
-        <li>Skills</li>
-        <li>Work</li>
-        <li>Contact</li>
-      </ul>
-
-      {/* Social Icons */}
-      <div></div>
-    </div>
+    <div></div>
   )
 }
 
