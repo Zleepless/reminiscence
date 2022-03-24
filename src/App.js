f90f302ae51457d38d082f9731208f09530360bf
Zleepless <donutsepia@gmail.com>
2022-03-25 03:10:30 +0700

    Test 2 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar"
+import Testcomp from './components/Testcomp'
 
 function App() {
   return (
     <div >
       <h1 class="text-3xl font-bold underline">Helasdaslo world!</h1>
       <Navbar />
+      <Testcomp />
     </div>
   )
 }
---
+++ src/components/Testcomp.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Testcomp = () => {
+  return (
+    <div>
+      <ul>
+        <li>Test1</li>
+        <li>Test2</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Testcomp
