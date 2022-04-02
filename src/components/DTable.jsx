1de053fc1b5c7958b930b2682fd3d09be8e22ff1
Zleepless <donutsepia@gmail.com>
2022-04-02 10:41:07 +0700

    Test React Datatable for Dev

--- src/components/DTable.jsx
+++ src/components/DTable.jsx
@@ -1,62 +1,81 @@
-
-import React from 'react';
+import React, { useState } from 'react';
 import DataTable from 'react-data-table-component';
 
-const columns = [
-    {
-        name: 'Title',
-        selector: row => row.title,
-        sortable: true,
-    },
-    {
-        name: 'Year',
-        selector: row => row.year,
-        sortable: true,
-    },
-];
-
-const customStyles = {
-    rows: {
-        style: {
-            minHeight: '40px', // override the row height
-        },
-    },
-    headCells: {
-        style: {
-            paddingLeft: '8px', // override the cell padding for head cells
-            paddingRight: '8px',
-        },
-    },
-    cells: {
-        style: {
-            paddingLeft: '8px', // override the cell padding for data cells
-            paddingRight: '8px',
-        },
-    },
-};
-
-const data = [
-    {
-        id: 1,
-        title: 'Beetlejuice',
-        year: '1988',
-    },
-    {
-        id: 2,
-        title: 'Ghostbusters',
-        year: '1984',
-    },
-]
-
 function DTable() {
+    const [hideDirector, setHideDirector] = React.useState(false);
+
+	const columns = React.useMemo(
+		() => [
+			{
+				name: 'Title',
+				selector: row => row.title,
+				sortable: true,
+			},
+			{
+				name: 'Director',
+				selector: row => row.director,
+				sortable: true,
+				omit: hideDirector,
+			},
+			{
+				name: 'Year',
+				selector: row => row.year,
+				sortable: true,
+			},
+		],
+		[hideDirector],
+	);
+
+    const data = [
+        {
+            id: 1,
+            title: 'Kongkadoo',
+            director: "Kong",
+            year: '2022',
+        },
+        {
+            id: 2,
+            title: 'Test',
+            director: "Kong",
+            year: '2021',
+        },
+    ]
+
+    const customStyles = {
+        rows: {
+            style: {
+                minHeight: '40px', // override the row height
+            },
+        },
+        headCells: {
+            style: {
+                paddingLeft: '8px', // override the cell padding for head cells
+                paddingRight: '8px',
+            },
+        },
+        cells: {
+            style: {
+                paddingLeft: '8px', // override the cell padding for data cells
+                paddingRight: '8px',
+            },
+        },
+    };
+
     return (
+        <div>
+        <button onClick={() => setHideDirector(!hideDirector)}>Hide Directory Column</button>
+        
         <DataTable
             columns={columns}
             data={data}
             selectableRows
             customStyles={customStyles}
+            pagination
         />
+        </div>
     );
 };
+
+
 
 export default DTable;
