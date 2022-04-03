a1e4cdb1470a87f7a95b4ca002f093047862ebfd
Zleepless <donutsepia@gmail.com>
2022-04-03 20:32:36 +0700

    Wash up for relearning

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react"
-import DTable from "./components/DTable"
 
 function App() {
   return (
     <div >
-      <h1 class="text-3xl font-bold underline">Helasdasloss world!</h1>
-      <DTable />
+
     </div>
   )
 }
